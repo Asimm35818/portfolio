230826cf84c392473b2b06fa87cdf047be19da21
Asimm35818 <53124707+Asimm35818@users.noreply.github.com>
2026-06-02 17:08:16 +0100

    Styled button corners and fixed icons in ProjectDetails.tsx

--- src/components/ProjectDetails.tsx
+++ src/components/ProjectDetails.tsx
@@ -59,39 +59,66 @@ const ProjectDetails: React.FC = () => {
               ))}
             </div>
 
-            <div className="mb-10 flex flex-wrap gap-3 sm:gap-4 sm:mb-12">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="12"
-                height="12"
-                viewBox="0 0 24 24"
-                fill="none"
-                stroke="currentColor"
-                strokeWidth="2"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                className="lucide lucide-github w-6 h-6 -mr-1.25"
-              >
-                <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
-                <path d="M9 18c-4.51 2-5-2-7-2"></path>
-                </svg>
-              <a
-                href={project.github}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-sm font-medium text-gray-700 transition-colors hover:text-gray-900 dark:text-gray-300 dark:hover:text-white sm:text-base"
-              >
-                View Code
-              </a>
-
-              <a
-                href={project.demo!}
-                target="_blank"
-                rel="noopener noreferrer"
-                className="text-sm font-medium text-gray-700 transition-colors hover:text-gray-900 dark:text-gray-300 dark:hover:text-white sm:text-base"
-              >
-                View Demo
-              </a>
+            <div className="mb-10 flex flex-wrap gap-3 sm:mb-12 sm:gap-4 ">
+              <div className="mb-10 flex flex-wrap gap-3 sm:mb-12 sm:gap-4 bg-blue-600 dark:bg-[#7aa2f7]">
+                <a
+                  href={project.github!}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="
+                inline-flex items-center gap-2
+                rounded-lg border border-(--background)
+                bg-(--background)
+                px-4 py-2
+                text-sm font-medium text-(--text)
+                transition-opacity hover:opacity-90
+                sm:text-base
+              "
+                >
+                  View Code
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    width="12"
+                    height="12"
+                    viewBox="0 0 24 24"
+                    fill="none"
+                    stroke="currentColor"
+                    strokeWidth="2"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="lucide lucide-github w-6 h-6 -mr-1.25"
+                  >
+                    <path d="M15 22v-4a4.8 4.8 0 0 0-1-3.5c3 0 6-2 6-5.5.08-1.25-.27-2.48-1-3.5.28-1.15.28-2.35 0-3.5 0 0-1 0-3 1.5-2.64-.5-5.36-.5-8 0C6 2 5 2 5 2c-.3 1.15-.3 2.35 0 3.5A5.403 5.403 0 0 0 4 9c0 3.5 3 5.5 6 5.5-.39.49-.68 1.05-.85 1.65-.17.6-.22 1.23-.15 1.85v4"></path>
+                    <path d="M9 18c-4.51 2-5-2-7-2"></path>
+                  </svg>
+                </a>
+              </div>
+              <div className="mb-10 flex flex-wrap gap-3 sm:mb-12 sm:gap-4 bg-blue-600 dark:bg-[#7aa2f7]">
+                {" "}
+                <a
+                  href={project.demo!}
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  className="
+                inline-flex items-center gap-2
+                rounded-lg border border-(--background)
+                bg-(--background)
+                px-4 py-2
+                text-sm font-medium text-(--text)
+                transition-opacity hover:opacity-90
+                sm:text-base
+              "
+                >
+                  View Demo
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 -960 960 960"
+                    className="h-5 w-5 fill-current"
+                  >
+                    <path d="M480-320 280-520l56-58 104 104v-326h80v326l104-104 56 58-200 200ZM240-160q-33 0-56.5-23.5T160-240v-120h80v120h480v-120h80v120q0 33-23.5 56.5T720-160H240Z" />
+                  </svg>
+                </a>
+              </div>
             </div>
 
             <div className="prose dark:prose-invert max-w-none">
