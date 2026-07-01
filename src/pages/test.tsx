b121a102d40b8cad8c3ec1771111f87334d2114d
Asimm35818 <53124707+Asimm35818@users.noreply.github.com>
2026-07-01 23:49:29 +0100

    Removed test.tsx

--- src/pages/test.tsx
+++
@@ -1,9 +0,0 @@
-import { useEffect } from "react";
-import ProjectDetails from "../components/ProjectDetails";
-import BlogPage from "../components/BlogPage";
-
-export default function Test() {
-  useEffect(() => {
-    document.title = "Blog";
-  }, []);
-}
